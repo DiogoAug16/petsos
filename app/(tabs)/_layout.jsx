879277365d606d92f3ee1c9150046b342328ff02
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2026-03-24 01:58:09 -0400

    refactor(): Convertido projeto de tsx para tsx e removido partes do template nao utilizado

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,28 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { useColorScheme } from '@/components/useColorScheme';
+import Colors from '@/constants/Colors';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme() ?? 'light';
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme].tint,
+        headerShown: true,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Mapa',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="map-outline" size={28} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}

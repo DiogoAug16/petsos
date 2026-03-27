7421549715b557bdecae66cb284deaca4a03e718
DiogoAug16 <113717493+DiogoAug16@users.noreply.github.com>
2026-03-27 19:38:28 -0400

    chore(constants): alterado colors de .js para .jsx

---
+++ constants/Colors.jsx
@@ -0,0 +1,19 @@
+const tintColorLight = '#FF6B35';
+const tintColorDark = '#FF6B35';
+
+export default {
+  light: {
+    text: '#1C1C1E',
+    background: '#F7F4F0',
+    tint: tintColorLight,
+    tabIconDefault: '#8A8A8E',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#8A8A8E',
+    tabIconSelected: tintColorDark,
+  },
+};

6513a9e2e6a4f2bc6334164263ea76c3fa96f653
Wesley Carvalho <wesleyamd@gmail.com>
2026-04-05 19:51:19 -0400

    fix: corrigir envio de múltiplas fotos na criação de denúncia

--- app/complaint/create.jsx
+++ app/complaint/create.jsx
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: 20,
     padding: 16,
     backgroundColor: COLORS.background,
     borderTopWidth: 1,

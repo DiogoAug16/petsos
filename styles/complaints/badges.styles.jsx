a8ea4548dc079426beb51efc0a3fe4ae142a07c5
DiogoAug16 <diogoaug16@outlook.com>
2026-04-10 20:04:14 -0400

    refactor: trocado a cor dos bagdes para facil visualização

--- styles/complaints/badges.styles.jsx
+++ styles/complaints/badges.styles.jsx
@@ -1,11 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 const ORANGE = '#FF6B35';
+const ORANGE_BG = 'rgba(255,107,53,0.1)';
 const DANGER_BG = 'rgba(230,57,70,0.1)';
 const DANGER_TEXT = '#A32D2D';
 const SUCCESS_BG = 'rgba(26,147,111,0.1)';
 const SUCCESS_TEXT = '#085041';
-const ORANGE_BG = 'rgba(255,107,53,0.1)';
+const abandono_bg = 'rgb(117, 177, 228, 0.1)'
+const abanadono_text = '#518abb'
+const animal_perdido_bg = 'rgb(241, 221, 72, 0.1)'
+const animal_perdido_text = '#8a7c0e'
+const maus_tratos_bg = 'rgba(230,57,70,0.1)';
+const maus_tratos_text = '#A32D2D';
+const negligencia_bg = 'rgba(237, 102, 114, 0.1)';
+const negligencia_text = '#aa4a4a';
+
 
 export const badgeStyles = (colorScheme) => {
   return StyleSheet.create({
@@ -23,19 +32,35 @@ export const badgeStyles = (colorScheme) => {
     },
 
     badgeAbandono: {
-      backgroundColor: DANGER_BG,
+      backgroundColor: abandono_bg,
     },
 
     badgeAbandonoText: {
-      color: DANGER_TEXT,
+      color: abanadono_text,
     },
 
     badgeNegligencia: {
-      backgroundColor: SUCCESS_BG,
+      backgroundColor: negligencia_bg,
     },
 
     badgeNegligenciaText: {
-      color: SUCCESS_TEXT,
+      color: negligencia_text,
+    },
+
+    badgeMausTratos: {
+      backgroundColor: maus_tratos_bg,
+    },
+
+    badgeMausTratosText: {
+      color: maus_tratos_text,
+    },
+
+    badgeAnimalPerdido: {
+      backgroundColor: animal_perdido_bg,
+    },
+
+    badgeAnimalPerdidoText: {
+      color: animal_perdido_text,
     },
 
     badgeOutro: {
